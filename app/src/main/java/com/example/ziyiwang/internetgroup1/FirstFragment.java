c793be87c1791da51a9f2b370f984ff6c5555337
Ziyi <slcfv3@gmail.com>
2018-09-16 23:52:36 -0400

    Second commit

---
+++ app/src/main/java/com/example/ziyiwang/internetgroup1/FirstFragment.java
@@ -0,0 +1,41 @@
+package com.example.ziyiwang.internetgroup1;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+
+    View myView;
+    private Button btn1;
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        myView=inflater.inflate(R.layout.fragment_first,container, false);
+        btn1=(Button) myView.findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(),NfcActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return myView;
+    }
+
+}
